09c4b4014e490c8e59418dd5f9e41033492fd1bb
MFJcode <mfjcode@gmail.com>
2021-12-19 07:03:12 +0100

    ValidaCPF.prototype.Valida = function() {

--- Validando um CPF.js
+++ Validando um CPF.js
@@ -8,3 +8,11 @@ function ValidaCPF(cpfEnviado) {
     });
 }
 
+ValidaCPF.prototype.Valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    return true;
+};
