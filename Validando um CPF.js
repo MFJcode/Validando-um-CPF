aad60a6f474131c92fd7ff17c7e96d29f046b626
MFJcode <mfjcode@gmail.com>
2021-12-20 09:26:00 +0100

    ValidaCPF.prototype.criaDigito

--- Validando um CPF.js
+++ Validando um CPF.js
@@ -11,8 +11,27 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.Valida = function() {
     if(typeof this.cpfLimpo === 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
-    return true;
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);    
 };
